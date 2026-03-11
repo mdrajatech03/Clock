ee28ef4762e590c20785f9c86a6e0ced690b8ad3
Md Raja <mdraja.tech.03@gmail.com>
2026-03-11 23:40:32 +0530

    Update script.js

--- script.js
+++ script.js
@@ -2,45 +2,37 @@ let hr = document.querySelector('#hr');
 let mn = document.querySelector('#mn');
 let sc = document.querySelector('#sc');
 
+setInterval(() => {
 
-setInterval(() =>{
-    let day = new Date();
-    let hh = day.getHours() * 30;
-    let mm = day.getMinutes() * 6;
-    let ss = day.getSeconds() * 6;
-    
-    hr.style.transform = `rotateZ(${hh+(mm/12)}deg)`;
-    mn.style.transform = `rotateZ(${mm}deg)`;
-    sc.style.transform = `rotateZ(${ss}deg)`; 
+let now = new Date();
 
+let hh = now.getHours() * 30;
+let mm = now.getMinutes() * 6;
+let ss = now.getSeconds() * 6;
 
+hr.style.transform = `rotateZ(${hh + (mm/12)}deg)`;
+mn.style.transform = `rotateZ(${mm}deg)`;
+sc.style.transform = `rotateZ(${ss}deg)`;
 
-    //  digital clock
-let hours = document.getElementById('hours');
-let minutes = document.getElementById('minutes');
-let seconds = document.getElementById('seconds');
-let ampm = document.getElementById('ampm');
+// DIGITAL CLOCK
 
-let h = new Date().getHours();
-let m = new Date().getMinutes();
-let s = new Date().getSeconds();
+let h = now.getHours();
+let m = now.getMinutes();
+let s = now.getSeconds();
 
 let am = h >= 12 ? "PM" : "AM";
 
-// convert 24hr clock to 12hr clock
-
 if(h>12){
-    h=h-12;
+h = h - 12;
 }
 
-// add zero before single digit number
+h = h < 10 ? "0"+h : h;
+m = m < 10 ? "0"+m : m;
+s = s < 10 ? "0"+s : s;
 
-h = (h < 10) ? "0" + h : h
-m = (m < 10) ? "0" + m : m
-s = (s < 10) ? "0" + s : s
+document.getElementById("hours").innerHTML = h;
+document.getElementById("minutes").innerHTML = m;
+document.getElementById("seconds").innerHTML = s;
+document.getElementById("ampm").innerHTML = am;
 
-hours.innerHTML = h;
-minutes.innerHTML = m;
-seconds.innerHTML = s;
-ampm.innerHTML = am;
-});
+},1000);
